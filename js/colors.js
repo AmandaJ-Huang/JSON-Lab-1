dbfcf9db7138325cc2abfe6b1235fcbd0ac7486f
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-11 22:54:55 -0500

    Update colors.js

--- js/colors.js
+++ js/colors.js
@@ -4,7 +4,7 @@ var colorsArray : {
             "name":"green",
             "red":"64",
             "green":"175",
-            "blue":"63",
+            "blue":"63"
         },
         {
             "name":"yellow",
@@ -17,17 +17,17 @@ var colorsArray : {
             "red":"192",
             "green":"46",
             "blue":"35"
-        }
+        },
         {
             "name":"blue",
             "red":"19",
             "green":"71",
-            "blue":"144",
+            "blue":"144"
         },
         {
             "name":"orange",
             "red":"253",
             "green":"92",
             "blue":"48"
-        },
+        }
     ]
